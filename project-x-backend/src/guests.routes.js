190d08db8c4ededf6c0374419c828b78fb0b8675
Davi Torelli <davitorelli@outlook.com>
2023-09-19 15:24:51 -0300

    estudando node js: crud de convidados

--- project-x-backend/src/guests.routes.js
+++ project-x-backend/src/guests.routes.js
@@ -7,70 +7,105 @@ const { response } = require("express");
 const prisma = new PrismaClient();
 
 // C
-guestsRoutes.post("/guests/store", async (request, response) => {
-    const { name, phone } = request.body;
-    const guest = await prisma.guest.create({
-        data: {
-            name,
-            phone
-        },
-    });
+guestsRoutes.post("/guest/store", async (request, response) => {
+  const { name, phone } = request.body;
+  const guest = await prisma.guest.create({
+    data: {
+      name,
+      phone,
+    },
+  });
 
-    return response.status(201).json(guest);
+  return response
+    .status(201)
+    .json({ response: true, message: "guest_created_ok", guest });
 });
 // R
 guestsRoutes.get("/guests", async (request, response) => {
-    const guests = await prisma.guest.findMany();
-    return response.status(200).json(guests);
+  const guests = await prisma.guest.findMany();
+  return response.status(200).json({ response: true, guests: guests });
 });
+
+guestsRoutes.get("/guest/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const intId = parseInt(id);
+
+  if (!intId) {
+    return response.status(400).json({ response: false, message: "id_error" });
+  }
+
+  const guest = await prisma.guest.findUnique({
+    where: { id: intId },
+  });
+
+  if (!guest) {
+    return response
+      .status(404)
+      .json({ response: false, message: "data_not_found" });
+  }
+
+  return response.status(200).json({ response: true, guest: guest });
+});
+
 // U
+guestsRoutes.patch("/guest/:id", async (request, response) => {
+  const { id } = request.params;
+  const intId = parseInt(id);
 
-guestsRoutes.put("/guests", async (request, response) => {
-    const { name, id, phone } = request.body;
+  if (!intId) {
+    return response.status(400).json({ response: false, message: "id_error" });
+  }
 
-    if (!id) {
-        return response.status(400).json("Id is mandatory");
-    }
+  const guestAlreadyExist = await prisma.guest.findUnique({
+    where: { id: intId },
+  });
 
-    const guestAlreadyExist = await prisma.guest.findUnique({ where: { id } });
+  if (!guestAlreadyExist) {
+    return response
+      .status(404)
+      .json({ response: false, message: "data_not_found" });
+  }
 
-    if (!guestAlreadyExist) {
-        return response.status(404).json("Guest not exist");
-    }
+  const { name, phone } = request.body;
 
-    const guest = await prisma.guest.update({
-        where: {
-            id,
-        },
-        data: {
-            name,
-            phone,
-        },
-    });
+  const guest = await prisma.guest.update({
+    where: {
+      id: intId,
+    },
+    data: {
+      name,
+      phone,
+    },
+  });
 
-    return response.status(200).json(guest);
+  return response.status(200).json({ response: true, message: "guest_updated_ok", guest });
 });
 // D
-guestsRoutes.delete("/guests/:id", async (request, response) => {
-    const { id } = request.params;
+guestsRoutes.delete("/guest/:id", async (request, response) => {
+  const { id } = request.params;
 
-    const intId = parseInt(id);
+  const intId = parseInt(id);
 
-    if (!intId) {
-        return response.status(400).json("Id is mandatory");
-    }
+  if (!intId) {
+    return response.status(400).json({ response: false, message: "id_error" });
+  }
 
-    const guestAlreadyExist = await prisma.guest.findUnique({
-        where: { id: intId },
-    });
+  const guestAlreadyExist = await prisma.guest.findUnique({
+    where: { id: intId },
+  });
 
-    if (!guestAlreadyExist) {
-        return response.status(404).json("Guest not exist");
-    }
+  if (!guestAlreadyExist) {
+    return response
+      .status(404)
+      .json({ response: false, message: "data_not_found" });
+  }
 
-    await prisma.guest.delete({ where: { id: intId } });
+  await prisma.guest.delete({ where: { id: intId } });
 
-    return response.status(200).send();
+  return response
+    .status(200)
+    .json({ response: true, message: "guest_deleted_ok" });
 });
 
 module.exports = guestsRoutes;
